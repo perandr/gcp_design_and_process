585d7db2a2985d8edfe0a7b533787c65fd7e9359
andrii Perun <perandr@gmail.com>
2019-08-25 00:51:39 +0300

    Added firewall rule for 80 port. Added debug info in startup script.

--- load_balancer/deployment-manager-examples/install-echo.sh
+++ load_balancer/deployment-manager-examples/install-echo.sh
@@ -3,7 +3,11 @@ ATTR=http://metadata.google.internal/computeMetadata/v1/instance/attributes
 apt-get update
 apt-get -y install python-pip
 python -m pip install --upgrade pip
+echo `curl -sf "${ATTR}/echo" -H "Metadata-Flavor: Google`
+echo `curl -sf "${ATTR}" -H "Metadata-Flavor: Google`
+
 if pkg=$(curl -sf "${ATTR}/echo" -H "Metadata-Flavor: Google"); then
+    echo $pkg
     python -m pip install $pkg
     gunicorn -b 0.0.0.0:80 -w 4 echo:app 
 fi
